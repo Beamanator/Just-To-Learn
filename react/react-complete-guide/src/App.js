16d0585c422922ff6425e268a9d51f3f0139c2b2
Beamanator <dabeamanator@gmail.com>
2018-03-02 18:27:41 +0200

    update for L42 - onclick listener

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -3,12 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Bill', age: 65},
+      { name: 'Steph', age: 33}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // DON'T USE THIS
+    // this.state.persons[0].name = "WOAH";
+    
+    // instead, do this:
+    this.setState({
+      persons: [
+        { name: 'JIMMAYY', age: 28 },
+        { name: 'Bill', age: 65},
+        { name: 'Steph', age: 33}
+      ]
+    }) 
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is working!</p>
-        <Person name="Max" age="28"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age="28"/>
         <Person name="Bill" age="65">Hobbies: Skating</Person>
         <Person name="Steph" age="34"/>
       </div>

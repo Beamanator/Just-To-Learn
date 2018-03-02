f558a133dbf3ac19a5a3d8e4970fa471d44d5ea3
Beamanator <dabeamanator@gmail.com>
2018-03-02 23:34:36 +0200

    update for L45 - onChange binding

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -11,15 +11,25 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // DON'T USE THIS
     // this.state.persons[0].name = "WOAH";
     
     // instead, do this:
     this.setState({
       persons: [
-        { name: 'JIMMAYY', age: 28 },
-        { name: 'Bill', age: 65},
+        { name: newName, age: 28 },
+        { name: 'BILLY', age: 65},
+        { name: 'Steph', age: 33}
+      ]
+    }) 
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Jim', age: 28 },
+        { name: event.target.value, age: 65},
         { name: 'Steph', age: 33}
       ]
     }) 
@@ -30,10 +40,16 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App!</h1>
         <p>This is working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age="28"/>
-        <Person name="Bill" age="65">Hobbies: Skating</Person>
-        <Person name="Steph" age="34"/>
+        <button onClick={this.switchNameHandler.bind(this, 'stephAY!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'TEST')}
+          changed={this.nameChangedHandler}
+        >Hobbies: Skating</Person>
+        <Person
+          name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }

5f28d30a3815772dfb913a65b08ba24c8224f62f
Beamanator <dabeamanator@gmail.com>
2018-03-02 15:31:41 +0200

    update for L33 - first custom component

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    // using JSX
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App!</h1>
-    //   </div>
-    // );
-
-    // using React library (JSX gets compiled into this)
-    // can render normal html element, or another custom component
-    return React.createElement('div',
-      {
-        className: 'App'
-      }, 
-      React.createElement('h1', null,'Hi, I\'m a React App!!!')
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!</h1>
+        <p>This is working!</p>
+        <Person />
+      </div>
     );
   }
 }

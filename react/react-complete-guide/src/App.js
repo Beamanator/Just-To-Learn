6f66da0e74654bab9b6133f50c92730af623c4c9
Beamanator <dabeamanator@gmail.com>
2018-04-03 02:50:19 +0200

    Updating to L67 - css modules

--- react/react-complete-guide/src/App.js
+++ react/react-complete-guide/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -93,14 +93,14 @@ class App extends Component {
         }
 
         // array of class names defined in App.css
-        const classes = [];
-        if (this.state.persons.length <= 2) classes.push('red');
-        if (this.state.persons.length <= 1) classes.push('bold');
+        const assignedClasses = [];
+        if (this.state.persons.length <= 2) assignedClasses.push( classes.red );
+        if (this.state.persons.length <= 1) assignedClasses.push( classes.bold );
 
         return (
-            <div className="App">
+            <div className={classes.App}>
                 <h1>Hi, I'm a React App!</h1>
-                <p className={classes.join(' ')}>This is working!</p>
+                <p className={assignedClasses.join(' ')}>This is working!</p>
                 <button
                     style={style} 
                     onClick={this.togglePersonsHandler}

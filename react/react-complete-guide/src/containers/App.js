48c258797ffdf4b5962c8c9ca199bed5d842b685
Beamanator <dabeamanator@gmail.com>
2018-04-07 22:39:23 +0200

    Updated for L81 - adding prop passed from index.js

--- react/react-complete-guide/src/containers/App.js
+++ react/react-complete-guide/src/containers/App.js
@@ -78,6 +78,7 @@ class App extends Component {
         return (
             <div className={classes.App}>
                 <Cockpit
+                    appTitle={this.props.title}
                     showPersons={this.state.showPersons}
                     persons={this.state.persons}
                     clicked={this.togglePersonsHandler}

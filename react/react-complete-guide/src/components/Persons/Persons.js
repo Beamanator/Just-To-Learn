6312ab4b76f0c9dcfa7d62bbf3f9560b7afae3c2
Beamanator <dabeamanator@gmail.com>
2018-04-09 01:56:33 +0200

    Update for L85 - Added creation lifecycle methods & console logs

--- react/react-complete-guide/src/components/Persons/Persons.js
+++ react/react-complete-guide/src/components/Persons/Persons.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Person from './Person/Person';
 
-const persons = (props) => {
-    return (
-        props.persons.map( (person, index) => (
-            <Person
-                click={() => props.clicked(index)}
-                name={person.name}
-                key={person.id}
-                age={person.age}
-                changed={(event) => props.changed(event, person.id)}>
-            </Person>
-        ))
-    );
+class Persons extends Component {
+    constructor(props) {
+        super(props);
+        console.log('[Persons.js] Inside Constructor', props);
+    }
+
+    componentWillMount() {
+        console.log('[Persons.js] Inside componentWillMount()');
+    }
+
+    componentDidMount() {
+        console.log('[Persons.js] Inside componentDidMount()');
+    }
+
+    render() {
+        console.log('[Persons.js] Inside render()');
+        
+        return (
+            this.props.persons.map( (person, index) => (
+                <Person
+                    click={() => this.props.clicked(index)}
+                    name={person.name}
+                    key={person.id}
+                    age={person.age}
+                    changed={(event) => this.props.changed(event, person.id)}>
+                </Person>
+            ))
+        );
+    }
 }
 
-export default persons;
+export default Persons;

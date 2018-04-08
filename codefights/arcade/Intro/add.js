7b9e5cd51f361ce67a5859bf0358df2214584b91
Beamanator <dabeamanator@gmail.com>
2018-04-08 03:02:09 +0200

    Added intro arcade soln

---
+++ codefights/arcade/Intro/add.js
@@ -0,0 +1,21 @@
+/**
+ * Name: Add
+ * Intro Arcade #: 1
+ * 
+ * Write a function that returns the sum of two numbers.
+ * 
+ * Example
+ * 
+ * For param1 = 1 and param2 = 2, the output should be
+ * add(param1, param2) = 3.
+ * 
+ * @param {any} param1 
+ * @param {any} param2 
+ * @returns 
+ */
+function add(param1, param2) {
+    return (param1 + param2);
+}
+
+// also can be written shorter as:
+add = (a, b) => a + b

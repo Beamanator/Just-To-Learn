73d87c5f6c969a66ed1b8665d198a33d7e1a7c43
Beamanator <dabeamanator@gmail.com>
2018-04-11 10:25:13 +0200

    new challenge soln

---
+++ codefights/challenges/psyOps.js
@@ -0,0 +1,23 @@
+/**
+ * Name: psyOps
+ * Reverse Challenge 11 Apr 2018
+ * 
+ * @param {any} a 
+ * @param {any} b 
+ * @returns 
+ */
+psyOps = (a, b) => ''
+    + (a + b)
+    + (a - b)
+    + a * b
+    + a / b
+
+// clean version:
+// function psyOps(a, b) {
+//     let first = a + b;
+//     let second = a - b;
+//     let third = a * b;
+//     let fourth = a / b;
+//
+//     return '' + first + second + third + fourth;
+// }

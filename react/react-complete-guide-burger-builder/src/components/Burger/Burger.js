4ebce7b0358d2be6f9c0a6bf13e87edd0645a979
Beamanator <dabeamanator@gmail.com>
2018-04-20 22:30:13 +0200

    Update for L118 - add warning when no ingredients selected

--- react/react-complete-guide-burger-builder/src/components/Burger/Burger.js
+++ react/react-complete-guide-burger-builder/src/components/Burger/Burger.js
@@ -5,13 +5,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])]
                 .map((_, i) =>
                     <BurgerIngredient key={igKey + i} type={igKey} />
                 );
-        });
+        })
+        // flatten array of arrays
+        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+        .reduce((accumulator, el) => {
+            return accumulator.concat(el);
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients =
+            <p>Please start adding ingredients!</p>;
+    }
 
     return (
         <div className={classes.Burger}>

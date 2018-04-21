865e391e01e4bd25969602d90c3b42f7b4619c5b
Beamanator <dabeamanator@gmail.com>
2018-04-21 15:26:18 +0200

    Update for L137 - added backdrop & removing SideDrawer

--- react/react-complete-guide-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ react/react-complete-guide-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Auxiliary from '../../../hoc/Auxiliary';
+
 import classes from './SideDrawer.css';
 
 const sideDrawer = (props) => {
+    let attachedClasses = [ classes.SideDrawer ];
+    attachedClasses.push(
+        props.open ? classes.Open : classes.Close
+    );
 
     return (
-        <div className={classes.SideDrawer}>
-            <div className={classes.Logo}>
-                <Logo />
+        <Auxiliary>
+            <Backdrop
+                show={props.open}
+                clicked={props.closed}
+            />
+            <div className={attachedClasses.join(' ')}>
+                <div className={classes.Logo}>
+                    <Logo />
+                </div>
+                <nav>
+                    <NavigationItems />
+                </nav>
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Auxiliary>
     );
 };
 

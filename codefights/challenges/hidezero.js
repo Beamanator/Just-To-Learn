a33c88e80f939168e342b314d6f8308ea1d5c5ba
Beamanator <dabeamanator@gmail.com>
2018-04-22 17:09:41 +0200

    Added challenge

---
+++ codefights/challenges/hidezero.js
@@ -0,0 +1,7 @@
+/**
+ * Name: hidezero
+ * Date: 22 Apr 2018
+ * Reverse Challenge
+ * but goal = return 1 for visible tests, 0 otherwise.
+ */
+hidezero = v => [1,3,100,1123,122009].includes(v) ? 1 : 0;

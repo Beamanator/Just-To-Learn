b6ee2d353d9a078a98ccc6aa681e1f06f126cfde
Beamanator <dabeamanator@gmail.com>
2018-05-12 16:15:27 +0200

    Update for L190 - accessing the correct post id within FullPost

--- react/react-http-guide/src/containers/Blog/FullPost/FullPost.js
+++ react/react-http-guide/src/containers/Blog/FullPost/FullPost.js
@@ -8,17 +8,18 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    componentDidUpdate() {
+    componentDidMount() {
+        console.log(this.props);
         // only do stuff if id is truthy
-        if (this.props.id) {
+        if (this.props.match.params.postId) {
 
             // only send get request if loadedPost exists & its id
             // is different from passed-in post id
             if (!this.state.loadedPost || (
                     this.state.loadedPost &&
-                    this.state.loadedPost.id !== this.props.id
+                    this.state.loadedPost.id !== this.props.match.params.postId
                 )) {
-                axios.get('/posts/' + this.props.id)
+                axios.get('/posts/' + this.props.match.params.postId)
                 .then(response => {
                     // console.log(response);
                     this.setState({
@@ -30,7 +31,7 @@ class FullPost extends Component {
     }
 
     deletePostHandler = () => {
-        axios.delete('/posts/' + this.props.id)
+        axios.delete('/posts/' + this.props.match.params.postId)
         .then(response => {
             console.log(response);
         });
@@ -38,7 +39,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
-        if (this.props.id) {
+        if (this.props.match.params.postId) {
             post = <p style={{textAlign: 'center'}}>Loading...!</p>;
         }
         if (this.state.loadedPost) {

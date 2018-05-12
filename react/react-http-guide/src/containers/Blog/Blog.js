48504d54d4cc4b92a15ed780ec2d5a93b2d1b5ce
Beamanator <dabeamanator@gmail.com>
2018-05-12 18:34:38 +0200

    Update for L202 - Lazy Routing / async loading components

--- react/react-http-guide/src/containers/Blog/Blog.js
+++ react/react-http-guide/src/containers/Blog/Blog.js
@@ -4,13 +4,22 @@ import React, { Component } from 'react';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
+// import NewPost from './NewPost/NewPost';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    // dynamic import syntax
+    // -> parameter is only imported when the function is executed
+    //  & this will only be executed when we render AsyncNewPost to
+    //  the screen
+    return import('./NewPost/NewPost');
+});
 
 import './Blog.css';
 
 class Blog extends Component {
     state = {
-        auth: false
+        auth: true
     }
 
     render () {
@@ -36,7 +45,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
                     <Route render={() => <h1>Not found</h1>} />
                     {/* <Redirect from="/" to="/posts" /> */}

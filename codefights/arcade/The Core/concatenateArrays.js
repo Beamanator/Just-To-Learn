db0af32eb0ae07dbe2d31ec4355fb2088fa3e84f
Beamanator <dabeamanator@gmail.com>
2018-05-19 20:20:45 +0200

    Added core soln

---
+++ codefights/arcade/The Core/concatenateArrays.js
@@ -0,0 +1,15 @@
+/**
+ * Name: Concatenate Arrays
+ * Core Arcade #: 38
+ * 
+ * Given two arrays of integers a and b, obtain the array formed by
+ * the elements of a followed by the elements of b.
+ * 
+ * Example
+ * 
+ * For a = [2, 2, 1] and b = [10, 11], the output should be
+ * concatenateArrays(a, b) = [2, 2, 1, 10, 11].
+ */
+function concatenateArrays(a, b) {
+    return a.concat(b)
+}

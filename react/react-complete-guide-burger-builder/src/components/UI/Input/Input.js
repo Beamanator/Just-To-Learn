065c8986ef5d25835b6f224a1d0f6480e31c3d24
Beamanator <dabeamanator@gmail.com>
2018-05-27 11:19:53 +0200

    Update for L232 - adding basic error message

--- react/react-complete-guide-burger-builder/src/components/UI/Input/Input.js
+++ react/react-complete-guide-burger-builder/src/components/UI/Input/Input.js
@@ -4,10 +4,17 @@ import classes from './Input.css';
 
 const input = (props) => {
     let inputElement = null;
+
+    let validationError = null;
     const inputClasses = [classes.InputElement];
 
     if (props.invalid && props.shouldValidate && props.touched) {
         inputClasses.push(classes.Invalid);
+        validationError = (
+            <p
+                className={classes.validationError}
+            >Please enter a valid value!</p>
+        );
     }
     
     switch (props.elementType) {
@@ -57,6 +64,7 @@ const input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     )
 }

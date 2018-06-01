bcac67411945de4814cd6a1305bc96d9b624d8c8
Beamanator <dabeamanator@gmail.com>
2018-06-01 11:49:15 +0200

    Update for L242 - Dispatching Actions

--- react/redux-sample-proj/redux-basics.js
+++ react/redux-sample-proj/redux-basics.js
@@ -8,15 +8,39 @@ const initialState = {
 
 // Reducer
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        // create new object with old state values!
+        // NOTE: Need to use transpiler like Babel to get ... to work
+        //       in node (see this Stack Overflow question: https://stackoverflow.com/questions/36666433/node-5-10-spread-operator-not-working)
+        // return {
+        //     ...state,
+        //     counter: state.counter + 1
+        // }
+        // INSTEAD: use Object.assign() which is effectively the same
+        return Object.assign({}, state, {
+            counter: state.counter + 1
+        });
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        // create new object with old state values!
+        // Note: can't use ... without babel (see above)
+        return Object.assign({}, state, {
+            counter: state.counter + action.value
+        });
+    }
+
     // return updated state
     return state;
 };
 
 // Store
 const store = createStore(rootReducer);
-
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
 
 // Subscription

bdbaa1268c89495a504b9e98d5579767de6403b2
Beamanator <dabeamanator@gmail.com>
2018-06-04 00:01:35 +0200

    Adding new solution - editing message w/ Embed

--- discord.js/test-so-question/index.js
+++ discord.js/test-so-question/index.js
@@ -54,7 +54,8 @@ client.on('message', message => {
                 // countMessages(message);
                 // sendSmallImg(message.channel);
                 // checkMessageType(message);
-                getNumUserReactions(message);
+                // getNumUserReactions(message);
+                editRichEmbedOnReaction(message);
                 break;
             
             case '?PING':
@@ -88,6 +89,53 @@ client.on('message', message => {
         }
     }
 });
+
+// https://stackoverflow.com/questions/50668366/discord-api-embed-message-doesnt-update
+function editRichEmbedOnReaction(message) {
+    const reactionFilter = (reaction, user) => reaction.emoji.name === '✅';
+
+    const embed = new Discord.RichEmbed({
+        title: 'Suggestion by someone',
+        description: 'This is a test suggestion. Can you please like it or dislike it :)',
+        fields: [
+            {name: 'Like:', value: '<3'}
+        ]
+    });
+
+    // add reaction emoji to message
+    message.channel.send(embed)
+    .then(msg => msg.react('✅'))
+    .then(mReaction => mReaction.message.react('❎') )
+    .then(mReaction => {
+        // createReactionCollector - responds on each react, AND again at the end.
+        const collector = mReaction.message
+            .createReactionCollector(reactionFilter, { time: 15000 });
+        
+        // set collector events
+        collector.on('collect', r => {
+            // immutably copy embed's Like field to new obj
+            let embedLikeField = Object.assign({}, embed.fields[0]);
+
+            // update 'field' with new value
+            embedLikeField.value = '<3 <3 <3';
+
+            // create new embed with old title & description, new field
+            const newEmbed = new Discord.RichEmbed({
+                title: embed.title,
+                description: embed.description,
+                fields: [ embedLikeField ]
+            });
+            
+            // edit message with new embed
+            // NOTE: can only edit messages you author
+            r.message.edit(newEmbed)
+            .then(newMsg => console.log(`new embed added`))
+            .catch(console.log);
+        });
+        collector.on('end', collected => console.log(`Collected ${collected.size} reactions`));
+    })
+    .catch(console.log);
+}
 
 // https://stackoverflow.com/questions/49206097/how-do-i-get-the-number-of-users-who-reacted-with-a-certain-emoji
 function getNumUserReactions(message) {

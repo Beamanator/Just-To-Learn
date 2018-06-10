9c97e7f7cdbc77335e5e757f05cd77a3550920cc
Beamanator <dabeamanator@gmail.com>
2018-06-10 11:52:34 +0200

    Update for L290 - initialize ingredients in burgerbuilder

--- react/react-complete-guide-burger-builder/src/store/reducers/burgerBuilder.js
+++ react/react-complete-guide-burger-builder/src/store/reducers/burgerBuilder.js
@@ -42,6 +42,19 @@ const reducer = (state = initialState, action) => {
                     INGREDIENT_PRICES[action.ingredientName]
             };
 
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: action.ingredients,
+                error: false
+            };
+
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return {
+                ...state,
+                error: true
+            };
+
         default:
             return state;
     }

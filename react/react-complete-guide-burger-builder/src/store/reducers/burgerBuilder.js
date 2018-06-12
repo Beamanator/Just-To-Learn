197d7ad342f46104ba1d63cb646f922abfd02f40
Beamanator <dabeamanator@gmail.com>
2018-06-12 17:13:09 +0200

    Update for L303 - refactoring reducers continued

--- react/react-complete-guide-burger-builder/src/store/reducers/burgerBuilder.js
+++ react/react-complete-guide-burger-builder/src/store/reducers/burgerBuilder.js
@@ -15,50 +15,58 @@ const INGREDIENT_PRICES = {
     bacon: 0.7
 };
 
+const addIngredient = (state, action) => {
+    const updatedIngredient = { [action.ingredientName]:
+        state.ingredients[action.ingredientName] + 1 }
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    
+    const updatedState = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice +
+            INGREDIENT_PRICES[action.ingredientName]
+    };
+    return updateObject(state, updatedState);
+};
+
+const removeIngredient = (state, action) => {
+    const updatedIngredient = { [action.ingredientName]:
+        state.ingredients[action.ingredientName] - 1 }
+    const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+    
+    const updatedStateR = {
+        ingredients: updatedIngredients,
+        totalPrice: state.totalPrice -
+            INGREDIENT_PRICES[action.ingredientName]
+    };
+    return updateObject(state, updatedStateR);
+}
+
+const setIngredients = (state, action) => {
+    return updateObject(state, {
+        ingredients: {
+            // method 1 for setting order ingredients display
+            //  on burger. Can also make a position property
+            salad: action.ingredients.salad,
+            bacon: action.ingredients.bacon,
+            cheese: action.ingredients.cheese,
+            meat: action.ingredients.meat
+        },
+        totalPrice: initialState.totalPrice,
+        error: false
+    });
+};
+
+const fetchIngredientsFailed = (state, action) => {
+    return updateObject(state, {error: true});
+};
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.ADD_INGREDIENT:
-            const updatedIngredient = { [action.ingredientName]:
-                state.ingredients[action.ingredientName] + 1 }
-            const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
-            
-            const updatedState = {
-                ingredients: updatedIngredients,
-                totalPrice: state.totalPrice +
-                    INGREDIENT_PRICES[action.ingredientName]
-            };
-            return updateObject(state, updatedState);
-
-        case actionTypes.REMOVE_INGREDIENT:
-            const updatedIngredientR = { [action.ingredientName]:
-                state.ingredients[action.ingredientName] - 1 }
-            const updatedIngredientsR = updateObject(state.ingredients, updatedIngredientR);
-            
-            const updatedStateR = {
-                ingredients: updatedIngredientsR,
-                totalPrice: state.totalPrice -
-                    INGREDIENT_PRICES[action.ingredientName]
-            };
-            return updateObject(state, updatedStateR);
-
-        case actionTypes.SET_INGREDIENTS:
-            return updateObject(state, {
-                ingredients: {
-                    // method 1 for setting order ingredients display
-                    //  on burger. Can also make a position property
-                    salad: action.ingredients.salad,
-                    bacon: action.ingredients.bacon,
-                    cheese: action.ingredients.cheese,
-                    meat: action.ingredients.meat
-                },
-                totalPrice: initialState.totalPrice,
-                error: false
-            });
-
-        case actionTypes.FETCH_INGREDIENTS_FAILED:
-            return updateObject(state, {error: true});
-        default:
-            return state;
+        case actionTypes.ADD_INGREDIENT: return addIngredient(state, action);
+        case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action);
+        case actionTypes.SET_INGREDIENTS: return setIngredients(state, action);
+        case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action);
+        default: return state;
     }
 }
 

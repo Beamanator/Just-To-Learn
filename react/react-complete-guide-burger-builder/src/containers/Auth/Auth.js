f61a6422626761cf4287d9429a4465d0be406832
Beamanator <dabeamanator@gmail.com>
2018-06-13 22:50:30 +0200

    Update for L310 - adding auth actions

--- react/react-complete-guide-burger-builder/src/containers/Auth/Auth.js
+++ react/react-complete-guide-burger-builder/src/containers/Auth/Auth.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 
 import classes from './Auth.css';
+import * as actions from '../../store/actions/index';
 
 class Auth extends Component {
     // state will be local, so don't handle in Redux
@@ -83,6 +85,15 @@ class Auth extends Component {
         this.setState({ controls: updatedControls });
     }
 
+    submitHandler = (event) => {
+        event.preventDefault();
+
+        this.props.onAuth(
+            this.state.controls.email.value,
+            this.state.controls.password.value
+        );
+    }
+
     render () {
         const formElementsArray = [];
         for (let key in this.state.controls) {
@@ -107,13 +118,19 @@ class Auth extends Component {
 
         return (
             <div className={classes.Auth}>
-                <form>
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
                 </form>
             </div>
         );
     }
-}
+};
 
-export default Auth;
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Auth);

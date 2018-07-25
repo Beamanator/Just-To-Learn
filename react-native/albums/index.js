aed8294e4161bfe0be8ba559785818ffd4f41154
Beamanator <dabeamanator@gmail.com>
2018-07-26 01:43:48 +0200

    changing to first basic component

--- react-native/albums/index.js
+++ react-native/albums/index.js
@@ -1,4 +1,17 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+// import { AppRegistry } from 'react-native';
+// import App from './App';
 
+// AppRegistry.registerComponent('albums', () => App);
+
+// MY CODE:
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+);
+
+// Render component to the device
 AppRegistry.registerComponent('albums', () => App);

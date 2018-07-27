fd2ee9b829c2c6ae3bc7a99ada322de95fec9e14
Beamanator <dabeamanator@gmail.com>
2018-07-27 02:28:02 +0200

    update for L31 - creating shell of album list

--- react-native/albums/index.js
+++ react-native/albums/index.js
@@ -6,12 +6,16 @@
 // MY CODE:
 // Import a library to help create a component
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import Header from './src/components/header';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 // Create a component
 const App = () => (
-    <Header headerText='Albums!' />
+    <View>
+        <Header headerText='Albums!' />
+        <AlbumList />
+    </View>
 );
 
 // Render component to the device

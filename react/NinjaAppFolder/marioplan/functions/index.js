00b52b3a05dbe2534ef294c5c51a97964aae56a9
Alex Beaman <dabeamanator@gmail.com>
2018-12-09 11:44:37 +0200

    MarioPlan - Update for L36 - cloud function firestore triggers

--- react/NinjaAppFolder/marioplan/functions/index.js
+++ react/NinjaAppFolder/marioplan/functions/index.js
@@ -1,10 +1,37 @@
 // Note: all of this code will be running on firebase server
 // -> NOT in the browser
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// after initializing the admin app, we can use
+// -> the firebase admin sdk moving forward :)
+admin.initializeApp(functions.config().firebase);
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
     response.send("Hello, ninjas!");
 });
+
+const createNotification = (notification) => {
+    return admin.firestore()
+        .collection('notifications')
+        .add(notification)
+        .then((document) => {
+            console.log('notification added', document);
+        })
+}
+
+// set up trigger for this function to run
+// -> trigger has to do with firestore
+// -> and reference to a specific document being created
+exports.projectCreated = functions.firestore
+    .document('/projects/{projectId}')
+    .onCreate((doc) => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName} ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp(),
+        };
+
+        return createNotification(notification);
+    });

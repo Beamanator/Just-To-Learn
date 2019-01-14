61f8322d43c9c378532529086f82d67e40485b0a
Beamanator <dabeamanator@gmail.com>
2019-01-14 10:32:08 +0200

    FBAuth L6,7 - log in / out

--- node/fb-auth-tut-ninja/scripts/auth.js
+++ node/fb-auth-tut-ninja/scripts/auth.js
@@ -17,3 +17,34 @@ signupForm.addEventListener('submit', (e) => {
         signupForm.reset(); // clear text from form
     })
 });
+
+// logout
+const logoutButton = document.querySelector('#logout');
+logoutButton.addEventListener('click', (e) => {
+    e.preventDefault(); // prevent... something
+    auth.signOut() // sign out current user
+    .then(() => {
+        console.log('user logged out');
+    })
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault(); // prevent refresh (which would close modal)
+
+    // get user info
+    // NOTE: can also do loginForm['login-email']...
+    const email = e.target['login-email'].value;
+    const password = e.target['login-password'].value;
+
+    // login user
+    auth.signInWithEmailAndPassword(email, password)
+    .then(cred => {
+        console.log(cred);
+        // get login modal & close & clear form
+        const modal = document.querySelector('#modal-login');
+        M.Modal.getInstance(modal).close(); // close modal
+        loginForm.reset(); // clear text from form
+    })
+});

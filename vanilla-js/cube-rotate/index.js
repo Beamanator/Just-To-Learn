3441c97de923156a8e5595b5338981d6d5dc8115
Alex Beaman <alex@insidenewcity.com>
2020-05-10 00:06:28 +0200

    uhh test stuff... going nowhere fast

--- vanilla-js/cube-rotate/index.js
+++ vanilla-js/cube-rotate/index.js
@@ -15,3 +15,15 @@ function changeSide() {
 changeSide();
 
 radioGroup.addEventListener("change", changeSide);
+cube.addEventListener("transitionend", function () {
+    console.log("transition end?");
+});
+
+let sides = [document.querySelector(".cube2.cube2__face--front").style];
+
+document.querySelector("#up").addEventListener("click", () => {
+    console.log("up");
+});
+document.querySelector("#down").addEventListener("click", () => {
+    console.log("down");
+});
